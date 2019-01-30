6b9fc0b0bf7997b50a715da0e48dff75a4cf8de0
Bfgeshka <bfgeshka@aaathats3as.com>
2019-01-30 15:41:53 +0900

    config.def.h

---
+++ config.def.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+/*
+ * How to get access token:
+ * https://vk.com/dev/auth_mobile
+ * recommended permissions: audio,video,docs,photos
+ *
+ * Use 'offline' permission if you want permanent token. Be careful with it.
+ * You can add your permanent token after '=' in CONST_TOKEN string.
+ */
+#define PERMISSIONS "video,docs,photos,offline"
+#define TOKEN_HEAD "&access_token="
+#define CONST_TOKEN "&access_token="
+#define REQ_HEAD "https://api.vk.com/method"
+
+#endif
