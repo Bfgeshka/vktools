0df773218a58a52ac612e7d8fe8e006cf5cfa9cb
Bfgeshka <bfgeshka@aaathats3as.com>
2019-01-30 15:47:56 +0900

    vkgrab.c: call for R_set-token()

--- src/vkgrab.c
+++ src/vkgrab.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "curlutils.h"
+#include "request_gen.h"
 
 int
 /*main ( int argc, char ** argv )*/
 main ( void )
 {
 	C_init();
+	R_set_token();
 
 	puts("oi");
 
