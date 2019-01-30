7d44554d81004516372634d8da3f116b21958469
Bfgeshka <bfgeshka@aaathats3as.com>
2019-01-30 16:20:18 +0900

    config.def.h: add application id

--- config.def.h
+++ config.def.h
@@ -2,6 +2,21 @@
 #define CONFIG_H_
 
 /*
+ * For generation of token getting link.
+ * Create your own application, get its ID and insert it here.
+ *
+ * For creating your application visit https://vk.com/editapp?act=create.
+ * Choose "standalone application" and give it a name you like.
+ *
+ * After go to "My apps": https://vk.com/apps?act=manage
+ * Go to 'manage' on the right side.
+ *
+ * Choose 'settings', and there you can see your application id.
+ * Copy it and replace value of APPLICATION_ID.
+ */
+#define APPLICATION_ID 5934008
+
+/*
  * How to get access token:
  * https://vk.com/dev/auth_mobile
  * recommended permissions: audio,video,docs,photos
