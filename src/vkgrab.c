b2457213b68bc5077cc7b2508df90e60bc323a06
Bfgeshka <bfgeshka@aaathats3as.com>
2019-01-31 14:47:02 +0900

    vkgrab.c: free account resources

--- src/vkgrab.c
+++ src/vkgrab.c
@@ -4,6 +4,7 @@
 #include "curlutils.h"
 #include "request_gen.h"
 #include "application.h"
+#include "account.h"
 
 int
 main ( int argc, char ** argv )
@@ -13,6 +14,7 @@ main ( int argc, char ** argv )
 
 	A_args( argc, argv );
 
+	AC_free();
 	C_finish();
 
 	return EXIT_SUCCESS;
