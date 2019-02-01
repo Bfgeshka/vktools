3abbeb747b14c005fedd13aad9679356f37ac2f7
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-01 23:07:21 +0900

    content_download.h

---
+++ include/content_download.h
@@ -0,0 +1,12 @@
+#ifndef CONTENT_DOWNLOADING_H_
+#define CONTENT_DOWNLOADING_H_
+
+/* Macros */
+#include "account.h"
+#include "json.h"
+#include <stdio.h>
+
+/* Protos */
+void DL_doc ( account * acc, json_t * el, FILE * log, long long post_id, long long comm_id );
+
+#endif
