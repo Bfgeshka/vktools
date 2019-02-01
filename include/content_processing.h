bf97f1bddbf9f2d6a602a159628a770321d77de5
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-01 11:06:20 +0900

    content_processing.h: guards

--- include/content_processing.h
+++ include/content_processing.h
@@ -1,3 +1,6 @@
+#ifndef CONTENT_PROCESSING_H_
+#define CONTENT_PROCESSING_H_
+
 /* Typedef */
 struct content
 {
@@ -10,3 +13,5 @@ struct content
 /* Protos */
 void CT_default ( void );
 void CT_print_types ( void );
+
+#endif
