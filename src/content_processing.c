414c0ed28dbbf0e134f517a6b1189bfe1351246b
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-01 15:06:59 +0900

    content_processing.c: get_albums()

--- src/content_processing.c
+++ src/content_processing.c
@@ -1,6 +1,9 @@
 /* Macros */
 #include "content_processing.h"
 #include "stringutils.h"
+#include "account.h"
+#include "json.h"
+#include "request_gen.h"
 #include <stdio.h>
 
 #define DEFAULT_CONTENT_DOCS 1
@@ -41,4 +44,48 @@ CT_print_types ( void )
 
 	puts(str->s);
 	free_string(str);
+}
+
+
+//size_t
+void
+get_albums ( account * acc )
+{
+	string * apimeth = construct_string(1024);
+	stringset( apimeth, "photos.getAlbums?owner_id=%lld&need_system=1", acc->id );
+
+	if ( acc->id < 0 )
+		stringcat( apimeth, "%s", "&album_ids=-7" );
+
+	int err_ret = 0;
+	json_t * json = RQ_request( apimeth, &err_ret );
+	free_string(apimeth);
+	if ( err_ret < 0 )
+		return;
+
+	long long counter = js_get_int( json, "count" );
+
+	if ( counter > 0 )
+	{
+		acc->albums = malloc( counter * sizeof(album) );
+		json_t * al_items;
+		al_items = json_object_get( json, "items" );
+//		albums = malloc( counter * sizeof(struct data_album) );
+		printf( "\nAlbums: %lld.\n", counter );
+
+		json_t * el;
+		size_t index;
+		json_array_foreach( al_items, index, el )
+		{
+			acc->albums[index].id = js_get_int( el, "id" );
+			acc->albums[index].size = js_get_int( el, "size" );
+			acc->albums[index].title = construct_string(1024);
+			stringset( acc->albums[index].title, "%s", js_get_str( el, "title" ) );
+//			photos_count += acc->albums[index].size;
+		}
+	}
+	else
+		puts( "No albums found." );
+
+	json_decref(json);
 }
