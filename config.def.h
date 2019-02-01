843b96dcb89e79946e26fd6e860318b928a63348
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-01 17:10:07 +0900

    config.def.h: add 'wall' permission

--- config.def.h
+++ config.def.h
@@ -24,7 +24,7 @@
  * Use 'offline' permission if you want permanent token. Be careful with it.
  * You can add your permanent token after '=' in CONST_TOKEN string.
  */
-#define PERMISSIONS "video,docs,photos,offline"
+#define PERMISSIONS "video,docs,photos,offline,wall"
 #define TOKEN_HEAD "&access_token="
 #define CONST_TOKEN "&access_token="
 
