0b3ff7fc557e723c1411a488c8ed935f59d80fba
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-03 00:57:28 +0900

    CT_git_albums_files()

--- include/content_processing.h
+++ include/content_processing.h
@@ -21,5 +21,6 @@ void CT_get_wall ( account * acc );
 void CT_get_groups ( account * acc );
 void CT_get_friends( account * acc );
 void CT_get_docs ( account * acc );
+void CT_get_albums_files ( account * acc );
 
 #endif
